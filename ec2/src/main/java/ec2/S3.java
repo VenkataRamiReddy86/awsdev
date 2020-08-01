85b8bc0513ca74a3df148106186016c5c2c1e3d4
venka <venka@LAPTOP-5G27FOH7>
2020-08-01 11:49:23 +0530

    first commit

---
+++ ec2/src/main/java/ec2/S3.java
@@ -0,0 +1,57 @@
+package ec2;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class S3 {
+
+	public static void main(String[] args) {
+		String key="roletest.txt";
+		String bucketName="roletestvenkat";
+				
+		System.out.format("Downloading %s from S3 bucket %s...\n", key, bucketName);
+		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+		try {
+			// Get an object and print its contents.
+           
+            S3Object fullObject = s3.getObject(new GetObjectRequest(bucketName, key));
+            System.out.println("Content-Type: " + fullObject.getObjectMetadata().getContentType());
+            System.out.println("Content: ");
+            displayTextInputStream(fullObject.getObjectContent());
+		} catch (AmazonServiceException e) {
+		    System.err.println(e.getErrorMessage());
+		    System.exit(1);
+		} catch (FileNotFoundException e) {
+		    System.err.println(e.getMessage());
+		    System.exit(1);
+		} catch (IOException e) {
+		    System.err.println(e.getMessage());
+		    System.exit(1);
+
+	}
+	}
+	
+	private static void displayTextInputStream(InputStream input) throws IOException {
+        // Read the text input stream one line at a time and display each line.
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+
+}
